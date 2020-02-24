ced119ddedfcad229fb572bf56795a62d25ce673
Zealerr <felix.rosenberg@outlook.com>
2020-02-24 16:35:36 +0100

    refactored some javascript code

--- js/test.js
+++ js/test.js
@@ -1,31 +1,48 @@
 // checkedmarker variables
 const CHECK = "fa-check-circle";
 const UNCHECK = "fa-circle-thin";
-
-// function that adds an item in a specific list
-
-// function that starts addListItem if there is something written
-
-
-// list of all lists variable
+// list of all lists variable set to []
 let listsArray = [];
+
+// function to set listsArray to the locally stored data. If no data
+// is stored then listsArray is not changed
+function getData() {
+    if (JSON.parse(localStorage.getItem("data")) != null) {
+        listsArray = JSON.parse(localStorage.getItem("data"));
+    }
+}
+
+// function that sets localstorage variable to listsArray
+function setData() {
+    localStorage.setItem("data", JSON.stringify(listsArray));
+}
 
 // function that adds a new list
 const addList = list => {
+    getData();
     listsArray.push([list]);
-    localStorage.setItem("data", JSON.stringify(listsArray));
+    setData();
 }
 
 // function that adds a new item into a specific list
 const addItem = (listIndex, itemName) => {
-    if(listsArray[listIndex]){
+    getData();
+    if (listsArray[listIndex]) {
         listsArray[listIndex].push(itemName);
-        localStorage.setItem("data", JSON.stringify(listsArray));
+        setData();
     } else {
         console.log("There is no list at this index");
     }
 }
 
+// function that removes a list
+const removeList = (listIndex) => {
+    getData;
+    listsArray.splice(listIndex);
+    setData();
+}
+
+// function that clears all stored data and sets listsArray back to []
 function clearData() {
     listsArray = [];
     localStorage.clearData();
